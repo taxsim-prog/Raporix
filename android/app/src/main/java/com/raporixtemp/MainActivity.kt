52ac34e81d9b841222fb9b344b260e6d61e82774
İLHAN <taxsim@gmail.com>
2026-03-09 19:04:13 +0300

    package adi duzeltildi

--- android/app/src/main/java/com/raporixtemp/MainActivity.kt
+++ android/app/src/main/java/com/raporixtemp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "RaporixTemp"
+  override fun getMainComponentName(): String = "Raptor"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
